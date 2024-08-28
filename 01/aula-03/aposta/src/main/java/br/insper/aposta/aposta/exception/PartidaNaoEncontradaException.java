f214449688f798dd5c60a1bc106cac8d5ccd5c81
Beni Sliozbergas <125266060+Benibergas@users.noreply.github.com>
2024-08-28 09:55:29 -0300

    Delete 01/aula-03/aposta directory

--- 01/aula-03/aposta/src/main/java/br/insper/aposta/aposta/exception/PartidaNaoEncontradaException.java
+++
@@ -1,9 +0,0 @@
-package br.insper.aposta.aposta.exception;
-
-public class PartidaNaoEncontradaException extends RuntimeException {
-
-    public PartidaNaoEncontradaException(String mensagem) {
-        super(mensagem);
-    }
-
-}
