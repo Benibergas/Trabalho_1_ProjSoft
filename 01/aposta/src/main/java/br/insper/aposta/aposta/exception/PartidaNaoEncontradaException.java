7db4b71050223d7baba33d6abd53da9c1e7e22d5
Beni Sliozbergas <125266060+Benibergas@users.noreply.github.com>
2024-08-28 09:57:50 -0300

    ...

---
+++ 01/aposta/src/main/java/br/insper/aposta/aposta/exception/PartidaNaoEncontradaException.java
@@ -0,0 +1,9 @@
+package br.insper.aposta.aposta.exception;
+
+public class PartidaNaoEncontradaException extends RuntimeException {
+
+    public PartidaNaoEncontradaException(String mensagem) {
+        super(mensagem);
+    }
+
+}
